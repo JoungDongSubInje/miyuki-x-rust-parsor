141085b19a3a3929a77a20728a8a80bc5d242b1a
JoungDongSubInje <pcdongsub@gmail.com>
2026-07-01 16:54:23 +0900

    Refectoring)

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,23 @@
 use maud::{Markup, html};
 use x_post::*;
 
-pub struct HtmlPost;
+pub trait ToFormat{
+    fn from_v_to_html(v_markdown: Vec<MarkDown>, posts: XPostingList)
+}
 
-impl HtmlPost{
+impl ToFormat for maud{
     // vector가 들어 있는 데이터 구조를 HTML로 변환하는 메서드
-    pub fn from_v_to_html(posts: XPostingList) -> Vec<MarkDown> {
-        let mut v_markdown: Vec<MarkDown>= Vec::new();
+    fn from_v_to_html(posts: XPostingList) -> Vec<MarkDown> {
 
         for post in posts.postings{
-            // HTMl에 링크를 주입
-            let link= Url::get_url(&post.url);
+
+            // from Url struct to html markup on Html 
+            let link= Url::get_url(&post.url); 
             let markup= html!{
                 a href=(link) { "x_post" }
             };
-            let opt_markup= Option::from(markup);
-            let parsed= match opt_markup {
-                Some(markup) => markup,
-                _ => panic!("Parsing err on post struct to Html struct")
-            };
 
-            let markdown= MarkDown::new(parsed);
+            let markdown= MarkDown::new(markup);
             
             v_markdown.push(markdown);
         }
@@ -29,32 +26,15 @@ impl HtmlPost{
     }
 }
 
-struct MarkDown{
-    htmls: Vec<maud::PreEscaped<String>>,
-}
-
-impl MarkDown{
-    pub fn new(htmls: Html) -> Self{
-        MarkDown { htmls }
-    }
-
-    // HTML을 마크다운으로 변환하는 메서드
-    // fn to_markdown(&self, post: T) -> String {
-    //     let html_string = HtmlPosting::to_html();
-    //     // 간단한 변환 예시 (실제 변환 로직은 더 복잡할 수 있음)
-        
-    // }
-}
-
 fn main(){
-    // let v:Vec<usize>= Vec::new();
-    // let html: HtmlPosting<usize> = HtmlPosting::new(v);
+    let str_url= "/jds_invoker/123456789";
+    let x_post= XPosting::new(str_url);
 
-    let posts= x_post::XPostingList::new();
-    // let post_query
-    
-    HtmlPost::from_v_to_html(
-        x_post::KoreaXPosting::push_x_posting(posts, "/dongsub/123145648")
-    );
+    // Box<Vec<XPostingList>>    
+    let mut x_posts= XPostingList::new();
+    x_posts.add_x_post(x_post);
 
+    // tansfer html struct
+    let mut v_markdown: Vec<MarkDown>= Vec::new();
+    let html_posts: Vec<MarkDown>= HtmlPost::from_v_to_html(x_posts);
 }
